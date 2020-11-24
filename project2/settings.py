e8b6360f797a21cfb4ea386bec802b94009bb82a
harshagrawal30 <65588931+harshagrawal30@users.noreply.github.com>
2020-11-24 22:09:51 +0530

    Update settings.py

--- project2/settings.py
+++ project2/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path,os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-#STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
