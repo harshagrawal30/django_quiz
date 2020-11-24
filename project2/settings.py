3a81557b01bde6070af24d0cd283ca046d3fbcd7
harshagrawal30 <65588931+harshagrawal30@users.noreply.github.com>
2020-11-24 20:15:49 +0530

    Update settings.py

--- project2/settings.py
+++ project2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o)buv76govwis=b#6_wjwc09b_siqgsqtp98kqf%4g^4l5g02_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','testquizdjango.herokuapp.com']
 
 
 # Application definition
