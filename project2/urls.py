b4897fcdad820517dc8e0f1bb13931a9f0705498
harshagrawal30 <65588931+harshagrawal30@users.noreply.github.com>
2020-11-24 20:42:53 +0530

    Update urls.py

--- project2/urls.py
+++ project2/urls.py
@@ -29,4 +29,4 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
-]
+
