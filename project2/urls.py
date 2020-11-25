f285d5df21eecfdbe98e5293bf90f181570585d0
harshagrawal30 <65588931+harshagrawal30@users.noreply.github.com>
2020-11-25 14:38:42 +0530

    Update urls.py

--- project2/urls.py
+++ project2/urls.py
@@ -17,8 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from app1 import urls
 from django.conf import settings
-from django.views.static import serve
-from django.conf.urls.static import static
+
 from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
